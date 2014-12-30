fb5c2f5297b914525ded4f1d6d1d0aaaf0512ab3
sbb <601367322@qq.com>
2014-12-30 18:19:05 +0800

    new

--- src/com/xl/controller/b/MessageServlet.java
+++ src/com/xl/controller/b/MessageServlet.java
@@ -3,16 +3,26 @@ package com.xl.controller.b;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.AttributeKey;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import net.sf.json.JSONObject;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.xl.bean.MessageBean;
 import com.xl.socket.HttpHelloWorldServerHandler;
@@ -21,7 +31,7 @@ import com.xl.util.MyUtil;
 import com.xl.util.ResultCode;
 
 @Controller
-@RequestMapping(value="/b")
+@RequestMapping(value = "/b")
 public class MessageServlet {
 
 	/**
@@ -40,11 +50,12 @@ public class MessageServlet {
 	public @ResponseBody
 	Object sendMessage(@RequestParam String content) {
 		System.out.println(content.toString());
-		MessageBean mb = (MessageBean) JSONObject.toBean(
-				JSONObject.fromObject(content), MessageBean.class);
+		MessageBean mb = (MessageBean) JSONObject.toBean(JSONObject
+				.fromObject(content), MessageBean.class);
 		JSONObject jo = new JSONObject();
 		if (HttpHelloWorldServerHandler.sessionMap.containsKey(mb.getToId())) {
-			ChannelHandlerContext session = HttpHelloWorldServerHandler.sessionMap.get(mb.getToId());
+			ChannelHandlerContext session = HttpHelloWorldServerHandler.sessionMap
+					.get(mb.getToId());
 			JSONObject toJo = new JSONObject();
 			toJo.put(StaticUtil.ORDER, StaticUtil.ORDER_SENDMESSAGE);
 			toJo.put(StaticUtil.FROMID, mb.getFromId());
@@ -52,7 +63,7 @@ public class MessageServlet {
 			toJo.put(StaticUtil.CONTENT, mb.getContent());
 			toJo.put(StaticUtil.MSGID, "");
 			toJo.put(StaticUtil.TIME, MyUtil.dateFormat.format(new Date()));
-			session.writeAndFlush(toJo.toString()+"\n");
+			session.writeAndFlush(toJo.toString() + "\n");
 
 			jo.put(ResultCode.STATUS, ResultCode.SUCCESS);
 			jo.put(StaticUtil.TIME, new Date());
@@ -77,20 +88,23 @@ public class MessageServlet {
 		JSONObject jo = new JSONObject();
 		if (HttpHelloWorldServerHandler.sessionMap.containsKey(deviceId)) {
 			if (HttpHelloWorldServerHandler.queueSessionMap.size() > 0
-					&& !HttpHelloWorldServerHandler.queueSessionMap.containsKey(deviceId)) {
+					&& !HttpHelloWorldServerHandler.queueSessionMap
+							.containsKey(deviceId)) {
 				String key = getKeyByDeviceId(deviceId);
-				ChannelHandlerContext session = HttpHelloWorldServerHandler.queueSessionMap.get(key);// 得到对方的session
+				ChannelHandlerContext session = HttpHelloWorldServerHandler.queueSessionMap
+						.get(key);// 得到对方的session
 
 				/** 将id添加到各自的session中 **/
 				setAttribute(session, deviceId);
-				setAttribute(HttpHelloWorldServerHandler.sessionMap.get(deviceId), key);
+				setAttribute(HttpHelloWorldServerHandler.sessionMap
+						.get(deviceId), key);
 
 				HttpHelloWorldServerHandler.queueSessionMap.remove(key);
 
 				JSONObject toJo = new JSONObject();
 				toJo.put(StaticUtil.ORDER, StaticUtil.ORDER_CONNECT_CHAT);
 				toJo.put(StaticUtil.OTHERDEVICEID, deviceId);
-				session.writeAndFlush(toJo.toString()+"\n");// 通知对方
+				session.writeAndFlush(toJo.toString() + "\n");// 通知对方
 
 				jo.put(ResultCode.STATUS, ResultCode.SUCCESS);
 				jo.put(StaticUtil.OTHERDEVICEID, key);
@@ -104,7 +118,7 @@ public class MessageServlet {
 		}
 		return jo;
 	}
-	
+
 	/**
 	 * 退出等待聊天列队
 	 * 
@@ -127,16 +141,19 @@ public class MessageServlet {
 	Object closeChat(@RequestParam String deviceId) {
 		JSONObject jo = new JSONObject();
 		if (HttpHelloWorldServerHandler.sessionMap.containsKey(deviceId)) {
-			ChannelHandlerContext session = HttpHelloWorldServerHandler.sessionMap.get(deviceId);
-			ArrayList<String> ids=(ArrayList<String>) session.attr(AttributeKey.valueOf(StaticUtil.IDS)).get();
-			if(ids!=null){
+			ChannelHandlerContext session = HttpHelloWorldServerHandler.sessionMap
+					.get(deviceId);
+			ArrayList<String> ids = (ArrayList<String>) session.attr(
+					AttributeKey.valueOf(StaticUtil.IDS)).get();
+			if (ids != null) {
 				for (String string : ids) {
-					ChannelHandlerContext temp = HttpHelloWorldServerHandler.sessionMap.get(string);
-					if(temp!=null){
+					ChannelHandlerContext temp = HttpHelloWorldServerHandler.sessionMap
+							.get(string);
+					if (temp != null) {
 						JSONObject toJo = new JSONObject();
 						toJo.put(StaticUtil.ORDER, StaticUtil.ORDER_CLOSE_CHAT);
 						toJo.put(StaticUtil.DEVICEID, deviceId);
-						temp.writeAndFlush(toJo.toString()+"\n");// 通知对方
+						temp.writeAndFlush(toJo.toString() + "\n");// 通知对方
 					}
 				}
 			}
@@ -149,8 +166,9 @@ public class MessageServlet {
 
 	public void setAttribute(ChannelHandlerContext session, String deviceId) {
 		ArrayList<String> ids = new ArrayList<String>();
-		if (session.attr(AttributeKey.valueOf(StaticUtil.IDS)).get()!=null) {
-			ids = (ArrayList<String>) session.attr(AttributeKey.valueOf(StaticUtil.IDS)).get();
+		if (session.attr(AttributeKey.valueOf(StaticUtil.IDS)).get() != null) {
+			ids = (ArrayList<String>) session.attr(
+					AttributeKey.valueOf(StaticUtil.IDS)).get();
 		}
 		ids.remove(deviceId);
 		ids.add(deviceId);
@@ -158,14 +176,36 @@ public class MessageServlet {
 	}
 
 	public String getKeyByDeviceId(String deviceId) {
-		int radom = (int) (Math.random() * HttpHelloWorldServerHandler.queueSessionMap.size());
-		String key = HttpHelloWorldServerHandler.queueSessionMap.keySet().toArray(
-				new String[HttpHelloWorldServerHandler.queueSessionMap.size()])[radom];
+		int radom = (int) (Math.random() * HttpHelloWorldServerHandler.queueSessionMap
+				.size());
+		String key = HttpHelloWorldServerHandler.queueSessionMap.keySet()
+				.toArray(
+						new String[HttpHelloWorldServerHandler.queueSessionMap
+								.size()])[radom];
 
 		if (key.equals(deviceId)) {
 			return getKeyByDeviceId(deviceId);
 		} else {
 			return key;
 		}
+	}
+
+	@RequestMapping(value = "/uploadfile")
+	public Object upload(HttpServletRequest request,@RequestParam("file") MultipartFile file) {
+		if (!file.isEmpty()) {
+			ServletContext sc = request.getSession().getServletContext();
+			String dir = sc.getRealPath("/upload"); // 设定文件保存的目录
+			String filename = file.getOriginalFilename(); // 得到上传时的文件名
+			try {
+				FileUtils.writeByteArrayToFile(new File(dir, filename), file
+						.getBytes());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("upload over. " + filename);
+		}
+		return "/test/success";
+
 	}
 }
